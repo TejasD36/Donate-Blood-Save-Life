7ef8955cac58f4978b635fc772df932fc6f3a4e4
Tejas Dudhal <116336036+TejasD36@users.noreply.github.com>
2023-03-19 13:59:22 +0530

    Add files via upload

---
+++ donatebloodsavelife.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 19, 2023 at 09:25 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `donatebloodsavelife`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donor`
+--
+
+CREATE TABLE `donor` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `dob` varchar(20) NOT NULL,
+  `contact` varchar(16) NOT NULL,
+  `save_life_date` varchar(20) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `blood_group` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `donor`
+--
+
+INSERT INTO `donor` (`id`, `name`, `gender`, `email`, `city`, `dob`, `contact`, `save_life_date`, `password`, `blood_group`) VALUES
+(54, 'Dudhal Tejas Pradip', 'Male', 'tejasdudhal05@gmail.com', 'baramati', '2002-11-05', '9146598209', '0', 'fcea920f7412b5da7be0cf42b8c93759', 'A+'),
+(55, 'Shri Pawar', 'Male', 'shri87@gmail.com', 'satara', '1971-04-21', '9146582031', '2023-03-19', 'e10adc3949ba59abbe56e057f20f883e', 'AB+');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `donor`
+--
+ALTER TABLE `donor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `donor`
+--
+ALTER TABLE `donor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
